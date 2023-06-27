f3736bef0025e1bde7bd26d0776437fb761cbf30
Vaibhav24y <118295704+Vaibhav24y@users.noreply.github.com>
2023-06-27 19:47:43 +0530

    Add files via upload

---
+++ assigment2.py
@@ -0,0 +1,37 @@
+x=2
+y=8
+print("add: ",x+y)
+print("sub: ",x-y)
+print("multi: ",x*y)
+print("div: ",x/y)
+print("floor div: ",x/y)
+a=6
+a+=2
+print("+=",a)
+a=10
+a-=1
+print("-=",a)
+a=10
+a%=1
+print("%=",a)
+a=8
+b=6
+print(">=",a>=b)
+print("<=",a<=b)
+print(">",a>b)
+d=False
+print("and",a and b)
+print("or",a or b)
+print("not",not b)
+a=6
+b=5
+print("bitwise &",a&b)
+print("bitwise |",a|b)
+print("bitwise ^",a^b)
+x=10
+y=5
+print("is",a is b)
+print("is not",a is not b)
+a="vaibhav"
+print('v'in a)
+print("a" not in a)
