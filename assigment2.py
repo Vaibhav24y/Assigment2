c70e72505a8ed4be793c1c2191d4560d00d39cd3
Vaibhav24y <118295704+Vaibhav24y@users.noreply.github.com>
2023-07-28 19:34:08 +0530

    Update assigment2.py

--- assigment2.py
+++ assigment2.py
@@ -1,37 +1,50 @@
-x=2
-y=8
-print("add: ",x+y)
-print("sub: ",x-y)
-print("multi: ",x*y)
-print("div: ",x/y)
-print("floor div: ",x/y)
-a=6
-a+=2
-print("+=",a)
-a=10
-a-=1
-print("-=",a)
-a=10
-a%=1
-print("%=",a)
 a=8
-b=6
-print(">=",a>=b)
-print("<=",a<=b)
+b=3
+Add=a+b
+print("addition is",Add)
+Sub=a-b
+print("subtraction is",Sub)
+Multip=a*b
+print("multiplication is",Multip)
+Division=a/b
+print("division is",Division)
+Exponential=a**b
+print("exponential is ",Exponential)
+Floordiv=a//b
+print("floor div is ",Floordiv)
+a=67
+a+=7
+print("+=",a)
+a=20
+a-=5
+print("-=",a)
+a=45
+a*=8
+print("*=",a)
+a=10
+b=50
+a>b
 print(">",a>b)
-d=False
-print("and",a and b)
-print("or",a or b)
-print("not",not b)
-a=6
-b=5
-print("bitwise &",a&b)
-print("bitwise |",a|b)
-print("bitwise ^",a^b)
-x=10
-y=5
-print("is",a is b)
-print("is not",a is not b)
-a="vaibhav"
-print('v'in a)
-print("a" not in a)
+a<b
+print("<",a<b)
+a==b
+print("==",a==b)
+a!=b
+print("!=",a!=b)
+a=True
+b=False
+print("and operation a and b:",a and b)
+print("or operation  a and b:",a or  b)
+print("not  operation  a and b:", not b ) 
+a=34
+b=50
+print("bitwise and operation between a and b:",a&b)
+print("bitwise or operation between a and b:",a|b)
+print("bitwise exponential operation between a and b:",a^b)
+x=67
+b=45
+print("is",x is b)
+print("is not", x is not b)
+x="vaibhav"
+print('x'in x)
+print("y"not in x)
